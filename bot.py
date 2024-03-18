1eceb8f2b2432c58101cb6b6c7034d465ee1fecd
kish <dhruvsharma3412@gmail.com>
2024-03-18 16:59:24 +0530

    initial commit

--- bot.py
+++ bot.py
@@ -1,1 +1,26 @@
 import discord
+from discord.ext import commands
+
+
+client = commands.Bot(
+    command_prefix = ">",
+    intents = discord.Intents.all(),
+    help_command = None,
+    strip_after_prefix = True,
+    case_insensitive = True
+)
+
+
+@client.event
+async def on_ready() -> None:
+
+    await client.tree.sync()
+    print(f"Logged in as {client.user}")
+
+
+@client.event
+async def on_message(message: discord.Message) -> None:
+    pass
+
+
+client.run("INSERT BOT TOKEN HERE")
