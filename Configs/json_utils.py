d605075977007f5e112f1ed04dc8d234a51b0abf
kish <dhruvsharma3412@gmail.com>
2024-03-21 09:53:49 +0530

    removed some functions that are no longer needed

--- Configs/json_utils.py
+++ Configs/json_utils.py
@@ -1,15 +1,5 @@
 import json
 
-def load_data() -> dict:
-    
-    with open("Database/config.json", "r") as file:
-        return json.load(file)
-    
-def save_data(data: dict) -> None:
-
-    with open("Database/config.json", "w") as file:
-        json.dump(data, file, indent = 4)
-
 def load_afk() -> dict:
     
     with open("Database/afk.json", "r") as file:
